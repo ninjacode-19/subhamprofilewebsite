5ac7f79bc083cc90e90fcedb34c8a6c3ca70daa8
Rupantar Mondal <rupantarkirnahar@gamil.com>
2022-08-18 00:16:26 +0530

    customize the website

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings:["Web Designer" , "Web Developer" , "Programmer", "Web Designer"],
+    strings:["" , "Pharmacist" , "Medical Representative ", ""],
     typeSpeed : 150,
     BackSpeed :60,
     loop : true 
 })
 // for about section typing animation
 var typed = new Typed(".typing-1",{
-    strings:["Web Designer" , "Web Developer" , "Programmer", "Web Designer"],
+    strings:["" , "Pharmacist" , "Medical Representative", ""],
     typeSpeed : 200,
     BackSpeed :60,
     loop : true 
